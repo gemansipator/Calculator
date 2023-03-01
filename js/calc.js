1e020388cfedb0784a77b1cee0912d63f2de84c6
gemansipator <gemansipator@gmail.com>
2023-03-01 17:04:12 +0400

    готово. решено с некоторыми пояснениями из разных источников

--- js/calc.js
+++ js/calc.js
@@ -9,9 +9,86 @@ const action = ['-', '+', '*','/'];
 // экран
 const out = document.querySelector('.calc-screen p');  // пояснение внизу №1.
 
+function clearAll () {
+    a = ''; // first number and result
+    b = ''; // second number
+    sign = ''; // знак
+    finish = false;
+    out.textContent = 0;
+}
+document.querySelector('.AC').onclick = clearAll; // в документе находим селектор AC
+//подключаем обработчик нажатий onclick и функцию clearAll которая будет срабатывать при нажатии
+
+document.querySelector('.buttons').onclick = (event) => {
+    // нажата кнопка
+    if (!event.target.classList.contains('btn')) return;
+    // нажата кнопка clearAll AC
+    if (event.target.classList.contains('AC')) return;
+
+    out.textContent = '';
+    // получаю нажатую кнопку
+    const key = event.target.textContent;
+
+    // если нажата клавиша 0-9 или ,
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+
+            out.textContent = a;
+        } else if (a !== '' && b !== '' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+
+        } else {
+            b += key;
+            out.textContent = b;
+        }
+        console.table(a, b, sign);
+        return;
+    }
+
+    //если нажата клавиша + - / *
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.table(a, b, sign);
+        return;
+    }
+
+    // нажата кнопка =
+    if (key === '=') {
+        if (b === '') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = a - b;
+                break;
+            case "*":
+                a = a * b;
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Ошибка';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = a / b;
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b, sign);
 
 
+    }
 
+
+}
 
 
 
